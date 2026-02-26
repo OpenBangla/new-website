0dc7efb792e89d922071cd1e9dd6da2663256022
Reshad Ahammed Ayon <codayon@proton.me>
2026-02-26 19:03:32 +0600

    chore: update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,50 +1,15 @@
-import Image from "next/image";
-import Button from "@/components/common/Button";
-import Container from "@/components/common/Container";
-import Flex from "@/components/common/Flex";
+import Capabilities from "./_components/Capabilities";
+import Features from "./_components/Features";
+import { GetObk } from "./_components/GetObk";
+import Hero from "./_components/Hero";
 
 export default function HomePage() {
   return (
-    <Container className="py-28">
-      <Flex className="justify-between">
-        <Flex className="flex-col items-baseline gap-6">
-          <h4 className="rounded-full bg-[#16CD8A]/25 px-5 py-2.5 font-medium text-[#06402B] text-sm">
-            Open Source • Cross Platform
-          </h4>
-
-          <h1 className="max-w-2xl font-bricolage-grotesque font-extrabold text-5xl text-[#202020]">
-            The Last{" "}
-            <span className="bg-linear-to-r from-[#0D8C5E] to-[#06402A] bg-clip-text text-transparent">
-              Bangla writing solution
-            </span>{" "}
-            you'll ever need!
-          </h1>
-
-          <p className="max-w-xl font-medium text-[#5D5D5D] text-xl leading-8">
-            Fast, smart Bangla typing with phonetic and fixed layouts Available
-            on
-            <span className="font-bold"> Linux, Windows, and macOS.</span>
-          </p>
-
-          <Flex className="gap-6">
-            <Button className="font-semibold">
-              Install OpenBangla Keyboard
-            </Button>{" "}
-            <Button className="border-[#5D5D5D] bg-transparent text-[#5D5D5D]">
-              View Demo
-            </Button>
-          </Flex>
-        </Flex>
-        <div>
-          <Image
-            src="https://placehold.co/500x300"
-            alt="placeholder"
-            height={300}
-            width={500}
-            loading="eager"
-          />
-        </div>
-      </Flex>
-    </Container>
+    <>
+      <Hero />
+      <Features />
+      <Capabilities />
+      <GetObk />
+    </>
   );
 }
