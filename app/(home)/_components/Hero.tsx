6c707091e7849412a60db907a9c4f232c3157b08
Reshad Ahammed Ayon <codayon@proton.me>
2026-02-26 18:49:05 +0600

    chore: move hero section to a component
    I am still not sure, how the project should be structured.

---
+++ app/(home)/_components/Hero.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Button from "@/components/common/Button";
+import Container from "@/components/common/Container";
+import Flex from "@/components/common/Flex";
+
+export default function Hero() {
+  return (
+    <section className="py-28">
+      <Container>
+        <Flex className="justify-between">
+          <Flex className="flex-col items-baseline">
+            <h4 className="rounded-full bg-[#16CD8A]/25 px-5 py-2.5 font-medium text-[#06402B] text-sm">
+              Open Source • Cross Platform
+            </h4>
+
+            <h1 className="leading-14 max-w-2xl pt-4 pb-5 font-bricolage-grotesque font-extrabold text-5xl text-[#202020]">
+              The Last{" "}
+              <span className="bg-linear-to-r from-[#0D8C5E] to-[#06402A] bg-clip-text text-transparent">
+                Bangla writing solution
+              </span>{" "}
+              you'll ever need!
+            </h1>
+
+            <p className="max-w-xl font-medium pb-12 text-[#5D5D5D] text-xl leading-8">
+              Fast, smart Bangla typing with phonetic and fixed layouts
+              Available on
+              <span className="font-bold"> Linux, Windows, and macOS.</span>
+            </p>
+
+            <Flex className="gap-6">
+              <Button className="font-semibold">
+                Install OpenBangla Keyboard
+              </Button>{" "}
+              <Button className="border-[#5D5D5D] bg-transparent text-[#5D5D5D]">
+                View Demo
+              </Button>
+            </Flex>
+          </Flex>
+          <div>
+            <Image
+              src="https://placehold.co/500x300"
+              alt="placeholder"
+              height={300}
+              width={500}
+              loading="eager"
+            />
+          </div>
+        </Flex>
+      </Container>
+    </section>
+  );
+}
